1de8ef2c5c9dbafc4c81986db9331a240a44ca97
Liliana Barbosa Alencar <106880411+Sigmaaustralis@users.noreply.github.com>
2025-04-24 17:04:24 -0300

    Update index.js
    atuaização

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AtendenteTela1 from './components/AtendenteTela1/AtendenteTela1';
 
 const App = () => {
   return (
     <div>
       <h1>Sistema de Atendimento de Senhas</h1>
       <p>Bem-vindo ao sistema de atendimento!</p>
+      {/* Renderizando a Tela do Atendente */}
+      <AtendenteTela1 />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
