e8acc9328325b79220ead995766219f753b857d4
Liliana Barbosa Alencar <106880411+Sigmaaustralis@users.noreply.github.com>
2025-04-24 14:47:26 -0300

    Create index.js
    criei o index.js

---
+++ frontend/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Sistema de Atendimento de Senhas</h1>
+      <p>Bem-vindo ao sistema de atendimento!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
